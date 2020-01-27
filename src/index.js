828a941305151675da1839bf9b9f4d7dd3e0a8f6
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-01-27 16:20:15 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
+console.log('hello');
+
 ReactDOM.render(<App />, document.getElementById('root'));
