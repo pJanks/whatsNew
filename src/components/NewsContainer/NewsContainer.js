0f5439018e7b20a7d5b8a4fa57d7d3fffdeee2e7
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-01-28 14:14:24 -0700

    Add initial structure for stories to render on the DOM

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './NewsContainer.css'
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+
+const NewsContainer = (props) => {
+  return props.ideas.map(newsArticle => {
+    return (<article key={newsArticle.id}>
+      <h2>{newsArticle.headline}</h2>
+      <img src={newsArticle.img}/>
+      <p>{newsArticle.description}</p>
+    </article>)
+  })
+}
 
 export default NewsContainer;
