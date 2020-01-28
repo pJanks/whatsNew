62c44201af0a815f2532ed19bad776588bbeecaf
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-01-28 14:12:56 -0700

    Add initial structure for stories to render on the DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
 import './App.css';
+import NewsContainer from '../NewsContainer/NewsContainer'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local: local
     }
   }
 
   render () {
-    return (
-      <div className="app">
-        YOUR CODE GOES HERE!
-      </div>
-    );
+    console.log(local);
+    return (<main>
+        <h1>What's News</h1>
+        <NewsContainer ideas={this.state.local}/>
+      </main>)
   }
 }
 
