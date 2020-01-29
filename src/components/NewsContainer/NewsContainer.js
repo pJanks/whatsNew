74d0f43b70a1b8158a23b9b2f95027dd27019c24
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-01-29 11:46:46 -0700

    Refactor components to match comp

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './NewsContainer.css'
+import NewsArticle from '../NewsArticle/NewsArticle'
 
 
 const NewsContainer = (props) => {
   return props.ideas.map(newsArticle => {
-    return (<article key={newsArticle.id}>
-      <h2>{newsArticle.headline}</h2>
-      <img src={newsArticle.img}/>
-      <p>{newsArticle.description}</p>
-    </article>)
+    return <NewsArticle article={newsArticle}/>
   })
 }
 
