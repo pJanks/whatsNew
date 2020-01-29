613af2e6cbd6c971318fada0e4d95e45cf0f87e4
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-01-29 11:44:42 -0700

    Add buttons and refactor components to match comp

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
+import Menu from '../Menu/Menu'
 import NewsContainer from '../NewsContainer/NewsContainer'
+// import NewsArticle from '../NewsArticle/NewsArticle'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local: local
+      local
     }
   }
 
   render () {
-    console.log(local);
     return (<main>
-        <h1>What's News</h1>
+      <h1>What's News</h1>
+        <Menu />
         <NewsContainer ideas={this.state.local}/>
       </main>)
   }
