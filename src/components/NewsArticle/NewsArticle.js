413ce206c253ace33c815a3927fef61606d0188c
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-01-29 11:46:25 -0700

    Refactor components to match comp

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+const NewsArticle = (props) => {
+  return (<article key={props.article.id}>
+    <h2>{props.article.headline}</h2>
+    <img src={props.article.img}/>
+    <p>{props.article.description}</p>
+  </article>)
+}
 
 export default NewsArticle;
