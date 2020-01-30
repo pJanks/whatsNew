aeedb2d14671926b18c23d455956c89efdc37d31
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-01-29 19:56:24 -0700

    Add more styling

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+
+  it('should match the snapshot with all data passed in correctly', () => {
+    const wrapper = shallow(<Card
+      title="Bananas"
+      description="B A N A N A S"
+      id={3}
+      removeIdea={jest.fn()}
+    />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
