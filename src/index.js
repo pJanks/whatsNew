1542b07758acd687cb9f4de3a0e30f0175280ee7
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-01-29 17:50:29 -0700

    Remove console log

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-console.log('hello');
 
 ReactDOM.render(<App />, document.getElementById('root'));
