1deb0d5adcc7e41e6e40ccdef3035c206ec34ec6
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-01-29 19:59:04 -0700

    Add initial test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -5,17 +5,17 @@ import { shallow } from 'enzyme';
 
 describe('App', () => {
 
-  it('should match the snapshot with all data passed in correctly', () => {
-    const wrapper = shallow(<Card
-      title="Bananas"
-      description="B A N A N A S"
-      id={3}
-      removeIdea={jest.fn()}
-    />);
+  it('should start with the expected states', () => {
+    const wrapper = shallow(<App />);
+    
+    expect(wrapper.state().local.length).toEqual(8);
+    expect(wrapper.state().entertainment.length).toEqual(8);
+    expect(wrapper.state().health.length).toEqual(8);
+    expect(wrapper.state().science.length).toEqual(8);
+    expect(wrapper.state().technology.length).toEqual(8);
+    expect(wrapper.state().currentPage.length).toEqual(8);
 
-    expect(wrapper).toMatchSnapshot();
   });
-});
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
